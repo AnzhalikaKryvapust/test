ca2dff6086380021bc68983ff917438c68126423
anzhalika.kryvapust <anzhalika.kryvapust@leverx.com>
2021-03-17 17:12:57 +0300

    constClass

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * {@link Main}
+ *
+ * @author Anzhalika Kryvapust
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("first class");
+    }
+}
